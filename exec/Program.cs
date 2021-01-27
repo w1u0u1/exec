e8aece760fc14da046ba51589c69d7a05a3566dc
w1u0u1 <78059024+w1u0u1@users.noreply.github.com>
2021-01-27 00:39:05 -0800

    Add files via upload

---
+++ exec/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+
+namespace exec
+{
+    class Program
+    {
+        static void error()
+        {
+            var error = new Win32Exception(Marshal.GetLastWin32Error()).Message;
+            Console.WriteLine(error);
+        }
+
+        static void exec(string cmdline)
+        {
+            win32.SECURITY_ATTRIBUTES sa = new win32.SECURITY_ATTRIBUTES();
+            win32.STARTUPINFO si = new win32.STARTUPINFO();
+            win32.PROCESS_INFORMATION pi = new win32.PROCESS_INFORMATION();
+
+            sa.nLength = Marshal.SizeOf(sa);
+            sa.lpSecurityDescriptor = IntPtr.Zero;
+            sa.bInheritHandle = true;
+
+            IntPtr hRead = IntPtr.Zero;
+            IntPtr hWrite = IntPtr.Zero;
+            if (!win32.CreatePipe(out hRead, out hWrite, ref sa, 0))
+                return;
+
+            si.cb = Marshal.SizeOf(si);
+            si.hStdError = hWrite;
+            si.hStdOutput = hWrite;
+            si.wShowWindow = win32.SW_HIDE;
+            si.dwFlags = win32.STARTF_USESHOWWINDOW | win32.STARTF_USESTDHANDLES;
+
+            var hToken = WindowsIdentity.GetCurrent().Token;
+            var hDupedToken = IntPtr.Zero;
+
+            if (!win32.DuplicateTokenEx(
+                                   hToken,
+                                  win32.GENERIC_ALL_ACCESS,
+                                   ref sa,
+                                   (int)win32.SECURITY_IMPERSONATION_LEVEL.SecurityIdentification,
+                                   (int)win32.TOKEN_TYPE.TokenPrimary,
+                                   ref hDupedToken
+                               ))
+            {
+                error();
+                return;
+            }
+
+            if (!win32.CreateProcessAsUser(
+                                                    hDupedToken,
+                                                    null,
+                                                    cmdline,
+                                                    ref sa, ref sa,
+                                                    true,
+                                                    win32.NORMAL_PRIORITY_CLASS | win32.CREATE_NO_WINDOW,
+                                                    IntPtr.Zero,
+                                                    null, ref si, ref pi
+                                            ))
+            {
+                error();
+                return;
+            }
+
+            win32.CloseHandle(hWrite);
+
+            while (true)
+            {
+                uint BytesRead = 0;
+                byte[] buf = new byte[10240];
+                if (!win32.ReadFile(hRead, buf, (uint)buf.Length, out BytesRead, IntPtr.Zero))
+                    break;
+                string str = Encoding.UTF8.GetString(buf, 0, (int)BytesRead);
+                Console.Write(str);
+                Thread.Sleep(100);
+            }
+
+            win32.CloseHandle(hRead);
+            win32.CloseHandle(pi.hProcess);
+            win32.CloseHandle(pi.hThread);
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 1)
+                    exec(args[0]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ exec/win32.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace exec
+{
+    class win32
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SECURITY_ATTRIBUTES
+        {
+            public int nLength;
+            public IntPtr lpSecurityDescriptor;
+            public bool bInheritHandle;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct STARTUPINFO
+        {
+            public Int32 cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        public enum SECURITY_IMPERSONATION_LEVEL
+        {
+            SecurityAnonymous,
+            SecurityIdentification,
+            SecurityImpersonation,
+            SecurityDelegation
+        }
+
+        public enum TOKEN_TYPE
+        {
+            TokenPrimary = 1,
+            TokenImpersonation
+        }
+
+        public enum TOKEN_INFORMATION_CLASS
+        {
+            /// <summary>
+            /// The buffer receives a TOKEN_USER structure that contains the user account of the token.
+            /// </summary>
+            TokenUser = 1,
+
+            /// <summary>
+            /// The buffer receives a TOKEN_GROUPS structure that contains the group accounts associated with the token.
+            /// </summary>
+            TokenGroups,
+
+            /// <summary>
+            /// The buffer receives a TOKEN_PRIVILEGES structure that contains the privileges of the token.
+            /// </summary>
+            TokenPrivileges,
+
+            /// <summary>
+            /// The buffer receives a TOKEN_OWNER structure that contains the default owner security identifier (SID) for newly created objects.
+            /// </summary>
+            TokenOwner,
+
+            /// <summary>
+            /// The buffer receives a TOKEN_PRIMARY_GROUP structure that contains the default primary group SID for newly created objects.
+            /// </summary>
+            TokenPrimaryGroup,
+
+            /// <summary>
+            /// The buffer receives a TOKEN_DEFAULT_DACL structure that contains the default DACL for newly created objects.
+            /// </summary>
+            TokenDefaultDacl,
+
+            /// <summary>
+            /// The buffer receives a TOKEN_SOURCE structure that contains the source of the token. TOKEN_QUERY_SOURCE access is needed to retrieve this information.
+            /// </summary>
+            TokenSource,
+
+            /// <summary>
+            /// The buffer receives a TOKEN_TYPE value that indicates whether the token is a primary or impersonation token.
+            /// </summary>
+            TokenType,
+
+            /// <summary>
+            /// The buffer receives a SECURITY_IMPERSONATION_LEVEL value that indicates the impersonation level of the token. If the access token is not an impersonation token, the function fails.
+            /// </summary>
+            TokenImpersonationLevel,
+
+            /// <summary>
+            /// The buffer receives a TOKEN_STATISTICS structure that contains various token statistics.
+            /// </summary>
+            TokenStatistics,
+
+            /// <summary>
+            /// The buffer receives a TOKEN_GROUPS structure that contains the list of restricting SIDs in a restricted token.
+            /// </summary>
+            TokenRestrictedSids,
+
+            /// <summary>
+            /// The buffer receives a DWORD value that indicates the Terminal Services session identifier that is associated with the token.
+            /// </summary>
+            TokenSessionId,
+
+            /// <summary>
+            /// The buffer receives a TOKEN_GROUPS_AND_PRIVILEGES structure that contains the user SID, the group accounts, the restricted SIDs, and the authentication ID associated with the token.
+            /// </summary>
+            TokenGroupsAndPrivileges,
+
+            /// <summary>
+            /// Reserved.
+            /// </summary>
+            TokenSessionReference,
+
+            /// <summary>
+            /// The buffer receives a DWORD value that is nonzero if the token includes the SANDBOX_INERT flag.
+            /// </summary>
+            TokenSandBoxInert,
+
+            /// <summary>
+            /// Reserved.
+            /// </summary>
+            TokenAuditPolicy,
+
+            /// <summary>
+            /// The buffer receives a TOKEN_ORIGIN value.
+            /// </summary>
+            TokenOrigin,
+
+            /// <summary>
+            /// The buffer receives a TOKEN_ELEVATION_TYPE value that specifies the elevation level of the token.
+            /// </summary>
+            TokenElevationType,
+
+            /// <summary>
+            /// The buffer receives a TOKEN_LINKED_TOKEN structure that contains a handle to another token that is linked to this token.
+            /// </summary>
+            TokenLinkedToken,
+
+            /// <summary>
+            /// The buffer receives a TOKEN_ELEVATION structure that specifies whether the token is elevated.
+            /// </summary>
+            TokenElevation,
+
+            /// <summary>
+            /// The buffer receives a DWORD value that is nonzero if the token has ever been filtered.
+            /// </summary>
+            TokenHasRestrictions,
+
+            /// <summary>
+            /// The buffer receives a TOKEN_ACCESS_INFORMATION structure that specifies security information contained in the token.
+            /// </summary>
+            TokenAccessInformation,
+
+            /// <summary>
+            /// The buffer receives a DWORD value that is nonzero if virtualization is allowed for the token.
+            /// </summary>
+            TokenVirtualizationAllowed,
+
+            /// <summary>
+            /// The buffer receives a DWORD value that is nonzero if virtualization is enabled for the token.
+            /// </summary>
+            TokenVirtualizationEnabled,
+
+            /// <summary>
+            /// The buffer receives a TOKEN_MANDATORY_LABEL structure that specifies the token's integrity level.
+            /// </summary>
+            TokenIntegrityLevel,
+
+            /// <summary>
+            /// The buffer receives a DWORD value that is nonzero if the token has the UIAccess flag set.
+            /// </summary>
+            TokenUIAccess,
+
+            /// <summary>
+            /// The buffer receives a TOKEN_MANDATORY_POLICY structure that specifies the token's mandatory integrity policy.
+            /// </summary>
+            TokenMandatoryPolicy,
+
+            /// <summary>
+            /// The buffer receives the token's logon security identifier (SID).
+            /// </summary>
+            TokenLogonSid,
+
+            /// <summary>
+            /// The maximum value for this enumeration
+            /// </summary>
+            MaxTokenInfoClass
+        }
+
+        [DllImport("advapi32.dll",
+              EntryPoint = "CreateProcessAsUser", SetLastError = true,
+              CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        public static extern bool
+          CreateProcessAsUser(IntPtr hToken, string lpApplicationName, string lpCommandLine,
+                              ref SECURITY_ATTRIBUTES lpProcessAttributes, ref SECURITY_ATTRIBUTES lpThreadAttributes,
+                              bool bInheritHandle, UInt32 dwCreationFlags, IntPtr lpEnvrionment,
+                              string lpCurrentDirectory, ref STARTUPINFO lpStartupInfo,
+                              ref PROCESS_INFORMATION lpProcessInformation);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool CreatePipe(out IntPtr hReadPipe, out IntPtr hWritePipe, ref SECURITY_ATTRIBUTES lpPipeAttributes, uint nSize);
+
+        [DllImport("kernel32.dll")]
+        public static extern void CloseHandle(IntPtr handle);
+
+        [DllImport("kernel32.dll", BestFitMapping = true, CharSet = CharSet.Ansi)]
+        public static extern bool ReadFile(IntPtr hFile, [Out] byte[] lpBuffer, uint nNumberOfBytesToRead, out uint lpNumberOfBytesRead, IntPtr lpOverlapped);
+
+        [DllImport("advapi32.dll", EntryPoint = "DuplicateTokenEx", SetLastError = true)]
+        public static extern bool DuplicateTokenEx(
+    IntPtr hExistingToken,
+    uint dwDesiredAccess,
+    ref SECURITY_ATTRIBUTES lpThreadAttributes,
+    Int32 ImpersonationLevel,
+    Int32 dwTokenType,
+    ref IntPtr phNewToken);
+
+        public const int SW_HIDE = 0;
+        public const int STARTF_USESHOWWINDOW = 0x00000001;
+        public const int STARTF_USESTDHANDLES = 0x00000100;
+        public const int CREATE_NEW_CONSOLE = 0x00000010;
+        public const uint CREATE_NO_WINDOW = 0x08000000;
+
+        public const uint NORMAL_PRIORITY_CLASS = 0x00000020;
+        public const uint GENERIC_ALL_ACCESS = 0x10000000;
+    }
+}
